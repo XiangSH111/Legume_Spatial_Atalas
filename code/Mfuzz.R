30f52e4a395dc84488aac30ec7a0f84d984ee64e
XiangSH111 <xiangsunhuan@163.com>
2026-08-11 14:41:37 +0800

    Update Mfuzz.R

--- code/Mfuzz.R
+++ code/Mfuzz.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library(Seurat)
 library(Mfuzz)
 
-protein <- read.delim("/data/MX_anno_exp.txt", row.names = 1, check.names = FALSE)
+protein <- read.delim("../data/MX_anno_exp.txt", row.names = 1, check.names = FALSE)
 
 protein <- as.matrix(protein)
 mfuzz_class <- new('ExpressionSet', exprs = protein)
@@ -18,7 +18,7 @@ mfuzz_class <- mfuzz_class[-na_rows, ]
 mfuzz_cluster <- mfuzz(mfuzz_class, c = cluster_num, m = mestimate(mfuzz_class))
 mfuzz.plot2(mfuzz_class, cl = mfuzz_cluster, mfrow = c(3,6), time.labels = colnames(protein))
 
-pdf("/output/MX_anno_exp_Mfuzz18.pdf", width = 20, height = 5, family = "GB1")
+pdf("../output/MX_anno_exp_Mfuzz18.pdf", width = 20, height = 5, family = "GB1")
 par(mar = c(0.5, 0.5, 0.2, 0.2), oma = c(0,0,0,0))
 
 p1 <- mfuzz.plot2(
@@ -40,11 +40,11 @@ cluster_size
 protein_cluster <- mfuzz_cluster$cluster
 protein_cluster <- cbind(protein[names(protein_cluster), ], protein_cluster)
 head(protein_cluster)
-write.table(protein_cluster, "/output/MX_anno_exp_gene_list_cluster18.txt", sep = '\t', col.names = NA, quote = FALSE)
+write.table(protein_cluster, "../output/MX_anno_exp_gene_list_cluster18.txt", sep = '\t', col.names = NA, quote = FALSE)
 
 protein_cluster <- mfuzz_cluster$cluster
 protein_standard <- mfuzz_class@assayData$exprs
 protein_standard_cluster <- cbind(protein_standard[names(protein_cluster), ], protein_cluster)
 head(protein_standard_cluster)
-write.table(protein_standard_cluster, "/output/MX_anno_exp_gene_list_standard_cluster18.txt", sep = '\t', col.names = NA, quote = FALSE)
+write.table(protein_standard_cluster, "../output/MX_anno_exp_gene_list_standard_cluster18.txt", sep = '\t', col.names = NA, quote = FALSE)
 
